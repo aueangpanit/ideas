1eff88ae81c7f19ed748f6030673f536f01e8238
aueangpanit <aueangpa@gmail.com>
2017-12-03 19:56:50 +0000

    working template

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    
+        'url' : 'mongodb://admin:Ir2EMHwLjo7BSoxt@cluster0-shard-00-00-mnxkm.mongodb.net:27017,cluster0-shard-00-01-mnxkm.mongodb.net:27017,cluster0-shard-00-02-mnxkm.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
+    
+    };
