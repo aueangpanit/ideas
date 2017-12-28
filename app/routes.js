9e80e17012863e5699d4497f77d905c067a9bdba
aueangpanit <aueangpa@gmail.com>
2017-12-28 11:07:57 +0700

    review and readmore added to info page

--- app/routes.js
+++ app/routes.js
@@ -1,30 +1,11 @@
 module.exports = function(app, passport, request) {
+        var apiKey = 'f5d33aa7f0ee6bdeae50b823541435cc';
     
         // =====================================
         // HOME PAGE (with login links) ========
         // =====================================
         app.get('/', function(req, res) {
-            var options = { method: 'GET',
-              url: 'https://api.themoviedb.org/3/search/multi',
-              qs: 
-               { include_adult: 'false',
-                 page: '1',
-                 query: 'new game',
-                 language: 'en-US',
-                 api_key: 'f5d33aa7f0ee6bdeae50b823541435cc' },
-              body: '{}' };
-            
-            request(options, function (error, response, body) {
-              if (error) throw new Error(error);
-                body = JSON.parse(body);
-              //console.log(body);
-              res.render('pages/index.ejs', { 
-                    body: {
-                        total_results: body.total_results,
-                        results: body.results
-                    }
-                });
-            });
+            res.render('pages/index.ejs');
         });
     
         // =====================================
@@ -32,30 +13,119 @@ module.exports = function(app, passport, request) {
         // =====================================
         app.get('/search', function(req, res) {
             var query = req.query.q;
+            if(query == null || query == "") { query = "#"; }
             var page = 1;
             if(req.query.p != null) {
                 page = req.query.p;
             }
 
-            var options = { method: 'GET',
-            url: 'https://api.themoviedb.org/3/search/multi',
-            qs: 
-            { include_adult: 'false',
-               page: page,
-               query: query,
-               language: 'en-US',
-               api_key: 'f5d33aa7f0ee6bdeae50b823541435cc' },
-            body: '{}' };
+            var options = { 
+                method: 'GET',
+                url: 'https://api.themoviedb.org/3/search/multi',
+                qs: { 
+                    include_adult: 'false',
+                    page: page,
+                    query: query,
+                    language: 'en-US',
+                    api_key: apiKey
+                },
+                body: '{}' 
+            };
           
             request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-              body = JSON.parse(body);
-            res.render('pages/search.ejs', {
+                if (error) throw new Error(error);
+                body = JSON.parse(body);
+                res.render('pages/search.ejs', {
                     q: query,
                     page: page,
                     total_pages: body.total_pages,
                     total_results: body.total_results,
                     results: body.results                 
+                });
+            });
+        });
+
+        // =====================================
+        // INFO ================================
+        // =====================================
+        app.get('/info', function(req, res) {
+            
+
+            var id = req.query.id;
+            var mediaType = req.query.mt;
+
+            var options = { 
+                method: 'GET',
+                url: 'https://api.themoviedb.org/3/movie/' + id,
+                qs: { 
+                    language: 'en-US',
+                    api_key: apiKey 
+                },
+                body: '{}' 
+            };
+            
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+                
+                var info = JSON.parse(body);
+
+                options = { method: 'GET',
+                url: 'https://api.themoviedb.org/3/movie/' + id + '/release_dates',
+                qs: { api_key: apiKey },
+                body: '{}' };
+                
+                request(options, function (error, response, body) {
+                    if (error) throw new Error(error);
+                
+                    body = JSON.parse(body);
+                    var certification = body.results[0].release_dates[0].certification;
+
+                    options = { method: 'GET',
+                    url: 'https://api.themoviedb.org/3/movie/' + id + '/credits',
+                    qs: { api_key: apiKey },
+                    body: '{}' };
+                    
+                    request(options, function (error, response, body) {
+                        if (error) throw new Error(error);
+                    
+                        body = JSON.parse(body);
+                        var cast;
+                        var maxCast = 8;
+                        if (body.cast.length > maxCast) {
+                            cast = new Array(maxCast);
+                            for (var i = 0; i < maxCast; i++) {
+                                cast[i] = body.cast[i];
+                            }
+                        }
+                        else {
+                            cast = body.cast;
+                        }
+
+                        options = { method: 'GET',
+                        url: 'https://api.themoviedb.org/3/movie/' + id + '/reviews',
+                        qs: 
+                         { page: '1',
+                           language: 'en-US',
+                           api_key: apiKey },
+                        body: '{}' };
+                      
+                        request(options, function (error, response, body) {
+                            if (error) throw new Error(error);
+                        
+                            body = JSON.parse(body);
+                            var reviews = body.results;
+
+                            res.render('pages/info.ejs', {
+                                info: info,
+                                media_type: mediaType,
+                                certification: certification,
+                                cast: cast,
+                                reviews: reviews
+                            });
+                        });
+
+
+                    });
                 });
             });
         });
